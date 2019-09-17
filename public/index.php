ff3abf8d8238584343468cee4721b13f81afe93d
PZon <piotrzon@gmail.com>
2019-09-17 12:43:22 +0200

    working saving user data into DB but unsolved bootstrap problem

--- public/index.php
+++ public/index.php
@@ -8,9 +8,9 @@ set_exception_handler('Core\Error::exceptionHandler');
 session_start();
 
 $router= new Core\Router();
-
 $router->add('{controller}/{action}');
 $router->add('',['controller'=>'Home', 'action'=>'index']);
+$router->add('register',['controller'=>'Register', 'action'=>'register']);
 
 $router->dispatch($_SERVER['QUERY_STRING']);
 	
