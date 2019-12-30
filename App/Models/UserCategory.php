5a1ac4004e78d775ff50055035691fa4b922f293
PZon <piotrzon@gmail.com>
2019-12-30 12:21:44 +0100

    working features add categories

--- App/Models/UserCategory.php
+++ App/Models/UserCategory.php
@@ -14,13 +14,13 @@ class UserCategory extends \Core\Model{
 	};
  }
 	public function saveCategory(){
-	 
+	
 	$this->validate();
 	
 	if(empty($this->errors)){
-	$this->category=filter_input(INPUT_POST,'category',FILTER_SANITIZE_STRING);
+	$category=filter_input(INPUT_POST,'category',FILTER_SANITIZE_STRING);
 	$this->expLimit=filter_input(INPUT_POST,'expLimit',FILTER_SANITIZE_NUMBER_INT);
-	 $category=strtoupper($this->category);
+	$category=strtoupper($this->category);
 	 if($this->categoryType=='UI'){
 		$sql="INSERT INTO user_in_cat VALUES (NULL, {$_SESSION['idUser']}, :category)";
 	 }else if($this->categoryType=='UE'){
@@ -32,21 +32,84 @@ class UserCategory extends \Core\Model{
 	 $stmt=$db->prepare($sql);
 	 
 	 $stmt->bindValue(':category', $category, PDO::PARAM_STR );
-	 $stmt->bindValue(':limit', $this->expLimit, PDO::PARAM_INT );
-	 
+	 if($this->categoryType=='UE'){
+	  $stmt->bindValue(':limit', $this->expLimit, PDO::PARAM_INT );
+	 }
 	 return $stmt->execute();
 	}
 	return false;
  }
  
  public function validate(){
-	 if ($this->category == '') {
+
+	if ($this->category == '') {
            $this->errors[] = 'Category name is required';
        }
-	 if ($this->expLimit < 0 || $this->expLimit > 100000) {
-           $this->errors[] = 'Value for expnses category should be set between 0 and 100000';
+	/*if(empty($category)){
+		   echo '<span class="formError">Category name is required!</span>'; 
+			$errorEmpty=true;
+	   }*/
+	   
+	 if(static::categoryExist($this->category, $this->categoryType)){
+		   $this->errors[]='Category name already exist';
+	   }
+	 if($this->categoryType=='UE'){
+	  if ($this->expLimit < 0 || $this->expLimit > 100000) {
+           $this->errors[] = 'Value for expenses category should be set between 0 and 100000';
        }
+	   if (!empty($this->expLimit)&&!is_numeric($this->expLimit)){
+           $this->errors[] = 'wrong field value';
+       }
+	 }
+ }
+ 
+ public static function findByCatName($catName, $type){
+	 $cat=strtoupper($catName);
+	 $sqlE="SELECT idCatE, nameCatE FROM ex_cat WHERE nameCatE = :catName
+	 UNION 
+	 SELECT idUserCatEx, nameUserCatEx FROM user_ex_cat 
+	 WHERE idUser={$_SESSION['idUser']} AND nameUserCatEx = :catName ";
+	 $sqlI="SELECT idCatI, nameCatI FROM in_cat WHERE nameCatI=:catName 
+	 UNION 
+	 SELECT idUserCatIn, nameUserCatIn FROM user_in_cat WHERE idUser={$_SESSION['idUser']} AND nameUserCatIn=:catName ";
+	 $sqlP="SELECT idCatPay, nameCatPay FROM pay_cat WHERE nameCatPay=:catName
+	 UNION 
+	 SELECT idUserCatPay, nameUserCatPay FROM user_pay_cat 
+	 WHERE idUser= {$_SESSION['idUser']} AND nameUserCatPay=:catName";
+	 $db=static::getDB();
 	 
+	 if($type=='UI'){
+		$stmt=$db->prepare($sqlI);
+	 }else if($type=='UE'){
+		$stmt=$db->prepare($sqlE);
+	 }else if($type=='UP'){
+		$stmt=$db->prepare($sqlP);
+	 }
+	 
+	 $stmt->bindValue(':catName', $cat, PDO::PARAM_STR);
+	 $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+	 $stmt->execute();
+	 
+	 return $stmt->fetch();
+ }
+ 
+  public static function categoryExist($catName, $type){
+	 $category=static::findByCatName($catName, $type);
+	 
+	 if($category){
+		return true;
+	 }
+	 return false;
+ }
+ 
+ public function getUserExpCat(){
+	 $sql="SELECT * FROM user_ex_cat WHERE idUser={$_SESSION['idUser']}";
+	 $db=static::getDB();
+	 $stmt=$db->prepare($sql);
+	 $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+	 $stmt->execute();
+	 $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+     return $result;
  }
 	
  }//end class
--- public/js/boot_scripts.js
+++ public/js/boot_scripts.js
@@ -19,7 +19,7 @@ function countdown(){
 	document.getElementById("watch").innerHTML=day+'/'+month+'/'+year+"<br>"+hour+':'+minute+':'+second;
 	setTimeout("countdown()",1000);
 }
-
+/*** Date picker ***/
 $(function() {
   $( "#datepickerFrom" ).datepicker({dateFormat: 'yy-mm-dd' }); 
   $( "#modalDatepickerFrom" ).datepicker({dateFormat: 'yy-mm-dd' }); 
@@ -29,3 +29,146 @@ $(function() {
   $( "#expenseDate" ).datepicker({dateFormat: 'yy-mm-dd' });
   $( "#transactionDate" ).datepicker({dateFormat: 'yy-mm-dd' });
 });
+
+/*** Expense Limit ***/
+ $(function(){
+	$("#addExpenseLimit").click(function(){
+	 $("#expenseLimit").toggle('slow');
+	});
+   });
+   
+
+$(function(){
+/*** Expense Cat Modal Validation ***/	
+ $('#btnExCat').click(function(){
+ event.preventDefault();
+	var categoryType=$('#catT').val();
+	var category=$('#exCat').val();
+	var expLimit=$('#expenseLimit').val();
+	var validation=true;
+
+	  if(category!=''){
+		$.ajax({
+		  url:"/category/verifyCat",
+		  method: "POST",
+		  data:{
+		    categoryType: categoryType,
+			category:category,
+			expLimit:expLimit
+		  },
+		  success:function(data){
+		    if(data != 'NoErrors'){
+				$("#infoCat").html('<span class="modalError">'+data+'</span>');
+				validation=false;
+			}else{
+			  $('#addExpenseCatModal').hide();
+			  location.reload();
+			  validation=true;
+			}
+		  }
+		});
+	  }else{
+		$("#infoCat").html('<span class="modalError">Category name is required!</span>');
+		validation=false;
+	  }
+	  
+	  if(validation==true){
+		$.ajax({
+		  url:"/category/create",
+		  method: "POST",
+		  data:{
+		    categoryType: categoryType,
+			category:category,
+			expLimit:expLimit
+		  }
+		});
+	  }  
+ });
+ /*** Payment type Modal Validation ***/
+ $('#btnPayCat').click(function(){
+ event.preventDefault();
+	var categoryType=$('#categoryType').val();
+	var category=$('#paymentCategory').val();
+	var validation=true;
+	  if(category!=''){
+		$.ajax({
+		  url:"/category/verifyCat",
+		  method: "POST",
+		  data:{
+		    categoryType: categoryType,
+			category:category
+		  },
+		  success:function(data){
+		    if(data != 'NoErrors'){
+				$("#infoCatP").html('<span class="modalError">'+data+'</span>');
+				validation=false;
+			}else if(data == 'NoErrors'){
+			  validation=true;
+			  $('#addPaymentTypeModal').modal('hide');
+			  location.reload();
+			}
+		  }
+		});
+	  }else{
+		$("#infoCatP").html('<span class="modalError">Category name is required!</span>');
+		validation=false;
+	  }
+	  
+	  if(validation==true){
+	    $.ajax({
+		  url:"/category/create",
+		  method: "POST",
+		  data:{
+		    categoryType: categoryType,
+			category:category
+		  }
+		});
+	  } 
+ });
+ 
+ /*** Income Cat Modal Validation ***/	
+ $('#btnIncCat').click(function(){
+	event.preventDefault();
+	var categoryType=$('#categoryType').val();
+	var category=$('#incomeCategory').val();
+	var validation=true;
+	  if(category!=''){
+		$.ajax({
+		  url:"/category/verifyCat",
+		  method: "POST",
+		  data:{
+		    categoryType: categoryType,
+			category:category
+		  },
+		  success:function(data){
+		    if(data != 'NoErrors'){
+				$("#infoCatI").html('<span class="modalError">'+data+'</span>');
+				validation=false;
+			}else if(data == 'NoErrors'){
+			  validation=true;
+			  $('#addIncomeCatModal').modal('hide');
+			  location.reload();
+			}
+		  }
+		});
+	  }else{
+		$("#infoCatI").html('<span class="modalError">Category name is required!</span>');
+		validation=false;
+	  }
+	  
+	  if(validation==true){
+	    $.ajax({
+		  url:"/category/create",
+		  method: "POST",
+		  data:{
+		    categoryType: categoryType,
+			category:category
+		  }
+		});
+	  } 
+ });
+ 
+ 
+});	
+
+
